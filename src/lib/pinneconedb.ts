3e2ffa64b642dcf86cf30015f3cd09d558bd39de
kesiddewa <kesiddewa600@gmail.com>
2024-07-04 20:52:09 +0700

    change information source, add vector databse connection, use langchain for optimize RAG

--- src/lib/pinneconedb.ts
+++ src/lib/pinneconedb.ts
@@ -1,3 +1,4 @@
+/* eslint-disable  @typescript-eslint/no-non-null-assertion */
 import { Pinecone } from "@pinecone-database/pinecone";
 import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
 import { PineconeStore } from "@langchain/pinecone";
